8a408a240ab62f15504458ac2801aceeb8b78040
ashwinihegde555 <56389774+ashwinihegde555@users.noreply.github.com>
2020-10-07 13:35:07 +0530

    Update deploy_dev.groovy

--- vars/deploy_dev.groovy
+++ vars/deploy_dev.groovy
@@ -1,35 +1,50 @@
 #!/usr/bin/env groovy
 
-node{
+def executeBuild(){
+    try{
     stage('SCM'){
     git 'https://github.com/ashwinihegde555/Maven_DB.git'
     }
+    } catch(Exception e){
+    throw e;
+    }
+    try{
     stage('Clean'){
         withMaven {
             sh "mvn clean"
         }
     }
+    }catch(Exception e){
+    throw e;
+    }
+    try{
       stage('Install'){
         withMaven {
             sh "mvn install"
         }
     }
+    }catch(Exception e){
+    throw e;
+    }
+    try{
+       
     stage('Build'){
        withMaven {
          
      sh "mvn package"
     }
+    }
+    }catch(Exception e){
+        throw e;
+    }
+
     stage('deploy'){
        echo  pwd()
          sshagent(['deploy_user']) {
        sh "scp  -o StrictHostKeyChecking=no target/RegApp2-0.0.1-SNAPSHOT.war ec2-user@13.233.244.215:/opt/apache-tomcat-8.5.58/webapps"
- }
-
-       
-    }
-  
- 
+         }  
     }
 }
+
 
   
